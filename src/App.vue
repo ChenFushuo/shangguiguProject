68dde80022411438dc644ad0143fc573d1dff951
chen_fushuo <3101757997@qq.com>
2023-09-15 13:31:22 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-const fn()=>{
+const fn = () => {
 
-}
+  console.log(
+      "kajsdfds"
+  )
+
+};
 </script>
 
 <template>
   <div>App根组件</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
