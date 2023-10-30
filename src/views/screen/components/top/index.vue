724e86596ac51e6e5e184d94cf74b6695586aa8d
chen_fuhsuo <3101757997@qq.com>
2023-10-30 17:44:21 +0800

    feat: Introduction of large-screen image files for data

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn">首页</span>
+    </div>
+    <div class="center"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.top {
+  width: 100%;
+  height: 40px;
+  display: flex;
+  .left {
+    flex: 1;
+    background: url("../../images/dataScreen-header-left-bg.png") no-repeat;
+    background-size: cover;
+
+    .lbtn {
+      width: 100px;
+      height: 40px;
+      float: right;
+    }
+  }
+  .center {
+    flex: 2;
+  }
+  .right {
+    flex: 1;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,14 +1,68 @@
 <template>
-  <el-button @click="goBack">返回</el-button>
+  <div class="container">
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <fiv class="left">左</fiv>
+        <fiv class="center">中</fiv>
+        <fiv class="right">右</fiv>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "vue-router";
-let $router = useRouter();
+import { onMounted, ref } from "vue";
+import Top from "./components/Top/index.vue";
+let screen = ref(); // 获取数据大屏盒子dom元素
 
-const goBack = () => {
-  $router.go(-1);
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+});
+
+//定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w;
+  const wh = window.innerHeight / h;
+  return ww < wh ? ww : wh;
+}
+
+//监听视口变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url("./images/bg.png") no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
